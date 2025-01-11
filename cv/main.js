2f4cd1a4be06c29410ed7e42e659df771740cc8b
BorisMatev <boris.matev72345@gmail.com>
2025-01-11 16:41:04 +0200

    Change form and add preview page

--- cv/main.js
+++ cv/main.js
@@ -1,22 +1,24 @@
-document.getElementById('imageInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        const imageUrl = e.target.result;
-        const imageContainer = document.getElementById('imageContainer');
-        imageContainer.style.backgroundImage = `url(${imageUrl})`;
-      };
-      reader.readAsDataURL(file);
-    }
-  });
+document.getElementById('imageInput').addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imageUrl = e.target.result;
+      localStorage.setItem('photo', imageUrl);
+      const imageContainer = document.getElementById('imageContainer');
+      imageContainer.style.backgroundImage = `url(${imageUrl})`;
+    };
+    reader.readAsDataURL(file);
+  }
+});
 function download() {
-    const element = document.getElementById('content');
-    html2pdf()
-        .from(element)
-        .save('myCV .pdf');
+  const element = document.getElementById('content');
+  html2pdf()
+    .from(element)
+    .save('myCV .pdf');
 }
 function update(value, IdForChange) {
-    var inputVal = value.value; // Get the input value
-    document.getElementById(IdForChange).innerText = inputVal; // Update the H1 text
+  var inputVal = value.value; // Get the input value
+  document.getElementById(IdForChange).innerText = inputVal; // Update the H1 text
+  localStorage.setItem(IdForChange, inputVal);
 }
