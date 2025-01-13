70add1384627f7297ca3f564067c0100575ae000
BorisMatev <boris.matev72345@gmail.com>
2025-01-13 17:29:35 +0200

    Format changes

--- cv/main.js
+++ cv/main.js
@@ -1,24 +1,58 @@
-document.getElementById('imageInput').addEventListener('change', function (event) {
-  const file = event.target.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const imageUrl = e.target.result;
-      localStorage.setItem('photo', imageUrl);
-      const imageContainer = document.getElementById('imageContainer');
-      imageContainer.style.backgroundImage = `url(${imageUrl})`;
-    };
-    reader.readAsDataURL(file);
-  }
+window.addEventListener('load', function () {
+  checkToken();
+  loadContent();
 });
+
+function checkToken() {
+  var token = localStorage.getItem("token");
+  if (!token) {
+    localStorage.clear();
+    window.location.href = "/welcome";
+  }
+}
+
+function loadContent() {
+  const ids = [
+    'name', 'lastName', 'email', 'phone', 'city', 'info', "hobbies", "language","network","link",
+    'workplace', 'place', 'year', 'job', 'details',
+    'workplace1', 'place1', 'year1', 'job1', 'details1',
+    'workplace2', 'place2', 'year2', 'job2', 'details2',
+    'class', 'place4', 'year4', 'school', 'details4',
+    'class1', 'place5', 'year5', 'school1', 'details5',
+    'class2', 'place6', 'year6', 'school2', 'details6'
+  ];
+  
+  ids.forEach(id => {
+    const value = localStorage.getItem(id);
+    if (value) {
+      document.getElementById(id).innerHTML = value;;
+  
+    }
+  });
+  
+  const savedImage = localStorage.getItem('photo');
+  if (savedImage) {
+    document.getElementById('imageContainer').style.backgroundImage = `url(${savedImage})`;
+    document.getElementById('dropdownMenuButton').style.backgroundImage = `url(${savedImage})`;
+  }
+}
+
+function update(value, IdForChange) {
+  var inputVal = value.value;
+  document.getElementById(IdForChange).innerText = inputVal;
+  localStorage.setItem(IdForChange, inputVal);
+}
+
 function download() {
   const element = document.getElementById('content');
-  html2pdf()
-    .from(element)
-    .save('myCV .pdf');
+  html2pdf().from(element).set({
+    filename: 'myCV.pdf',
+    html2canvas: { scale: 4 },
+    jsPDF: {orientation: 'portrait', unit: 'in', compressPDF: true}
+  }).save();
 }
-function update(value, IdForChange) {
-  var inputVal = value.value; // Get the input value
-  document.getElementById(IdForChange).innerText = inputVal; // Update the H1 text
-  localStorage.setItem(IdForChange, inputVal);
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "/welcome";
 }
